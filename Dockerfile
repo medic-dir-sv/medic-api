854dcb99b347ae58cd8b86ea7f5174cdc8861030
wmoralesdev <walterrafael26@gmail.com>
2022-07-03 21:16:00 -0600

    feat: added docker support for work

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
+
+WORKDIR /
+COPY credentials /
+
+WORKDIR /app
+
+# Copy everything
+COPY . .
+
+# Restore as distinct layers
+RUN dotnet restore
+
+# Build and publish a release
+RUN dotnet publish -c Release -o out
+WORKDIR /app/out
+
+EXPOSE 5000:5000
+# ENTRYPOINT ["dotnet", "Medic.API.dll"]
+CMD ASPNETCORE_URLS=http://*:$PORT dotnet Medic.API.dll
---
+++ Medic.Domain/Converters/TimeIntervalConverter.cs
@@ -0,0 +1,37 @@
+﻿using Medic.Domain.Entities.TimeMeasures;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Medic.Domain.Converters;
+
+public class TimeIntervalConverter : ValueConverter<IList<TimeInterval>?, string?>
+{
+    public TimeIntervalConverter() : base(le => TimeIntervalListToString(le),
+        s => StringToTimeIntervalList(s))
+    {
+    }
+
+    private static string? TimeIntervalListToString(IList<TimeInterval>? value)
+    {
+        if (value is null || !value.Any())
+            return null;
+
+        return String.Join(",", value);
+    }
+
+    private static IList<TimeInterval>? StringToTimeIntervalList(string? value)
+    {
+        if (value is null || value == string.Empty)
+            return null;
+
+        return value.Split(',').Select(i =>
+        {
+            var intervals = i.Split('-');
+
+            return new TimeInterval
+            {
+                StartsAt = TimeSpan.Parse(intervals[0]),
+                EndsAt = TimeSpan.Parse(intervals[1])
+            };
+        }).ToList();
+    }
+}
